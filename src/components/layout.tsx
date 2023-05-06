94f53bad33034c29e66a61023e7bce9458bd8187
Ayden Springer <37941646+WraithWinterly@users.noreply.github.com>
2023-05-06 18:07:33 -0400

    Enforce Arbitrum Goerli network

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,12 +1,21 @@
-import type { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import Header from "./layout/header";
 import Sidebar from "./layout/sidebar";
 import { useRouter } from "next/router";
+import { useNetwork, useSwitchNetwork } from "wagmi";
+import Modal from "./ui-blocks/Modal";
+import NoSSR from "react-no-ssr";
 
 export default function Layout({ children }: { children: ReactNode }) {
   const pathname = useRouter().pathname;
+  const [open, setOpen] = useState(false);
+
+  const { chain } = useNetwork();
+  const { chains, error, isLoading, pendingChainId, switchNetwork } =
+    useSwitchNetwork();
 
   if (pathname === "/logo") return <>{children}</>;
+
   return (
     <>
       <Sidebar />
@@ -14,6 +23,28 @@ export default function Layout({ children }: { children: ReactNode }) {
       <div className="flex min-h-screen w-full flex-col bg-gray-800 px-4 pb-20 pl-64 pt-28">
         {children}
       </div>
+      <NoSSR>
+        <Modal
+          isOpen={open}
+          setIsOpen={setOpen}
+          forceOpen={chain?.id != 421613}
+          title="Incorrect Network"
+        >
+          <div className="flex flex-col rounded-xl bg-red-900 p-4">
+            <span className="text-xl">WARNING:</span>
+            <b>
+              Do NOT send any transactions until you are on{" "}
+              <i>Arbitrum Goerli</i>
+            </b>
+            <span>
+              Current chain: {chain?.id} ({chain?.name})
+            </span>
+            <button className="btn mt-8" onClick={() => switchNetwork!(421613)}>
+              Switch to Arbitrum Goerli (421613)
+            </button>
+          </div>
+        </Modal>
+      </NoSSR>
     </>
   );
 }
---
+++ src/components/ui-blocks/Modal.tsx
@@ -0,0 +1,98 @@
+import { Dispatch, SetStateAction } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment } from "react";
+import { IoClose } from "react-icons/io5";
+import { ReactNode } from "react";
+
+interface ModalProps {
+  children: ReactNode;
+  isOpen: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+  title: string;
+  borderless?: boolean;
+  forceOpen?: boolean;
+}
+export function CloseButton({
+  setIsOpen,
+}: {
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+}) {
+  return (
+    <button
+      className="rounded-full bg-black/40 p-2 text-gray-200 transition-all hover:scale-105 hover:bg-black/80 hover:text-white"
+      onClick={() => {
+        setIsOpen(false);
+      }}
+    >
+      <IoClose size={32} />
+    </button>
+  );
+}
+
+export default function Modal({
+  children,
+  isOpen,
+  setIsOpen,
+  title,
+  borderless,
+  forceOpen,
+}: ModalProps) {
+  return (
+    <>
+      <Transition appear show={forceOpen ? true : isOpen} as={Fragment}>
+        <Dialog as="div" className="relative" onClose={() => setIsOpen(false)}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel
+                  className={`transform overflow-hidden rounded-2xl text-left align-middle text-white shadow-xl backdrop-blur-md transition-all ${
+                    borderless
+                      ? "m-0 w-full max-w-4xl bg-none p-0 backdrop-blur-none "
+                      : "w-fit bg-black/50 p-2"
+                  }`}
+                >
+                  {/* Title */}
+                  {!borderless && (
+                    <Dialog.Title
+                      as="h1"
+                      className="m-0 -ml-3 p-0 leading-6 text-white"
+                    >
+                      <div className="flex items-center justify-between text-white">
+                        <h4 className="px-4">{title}</h4>
+                        <div className="mr-2">
+                          {!forceOpen && <CloseButton setIsOpen={setIsOpen} />}
+                        </div>
+                      </div>
+                    </Dialog.Title>
+                  )}
+
+                  <div className={`${borderless ? "" : "p-1"}`}>{children}</div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  );
+}
